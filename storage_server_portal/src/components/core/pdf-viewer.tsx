71b4f3086da5be4032fe418f1466b05c100afc49
nadavshaked <nadav2sh@gmail.com>
2025-03-02 15:50:26 +0200

    remove files

--- storage_server_portal/src/components/core/pdf-viewer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-import type ReactPDF from '@react-pdf/renderer';
-
-export const PDFViewer = dynamic<ReactPDF.PDFViewerProps>(
-  () => import('@react-pdf/renderer').then((module) => module.PDFViewer),
-  { ssr: false }
-);
